72846267f65f3d908b094babab46876b64ebd2e2
Alexander Cyon <alex.cyon@gmail.com>
2020-04-20 00:49:07 +0200

    PlayerControllerComponent logic

--- Common/PlayerControlComponent.swift
+++ Common/PlayerControlComponent.swift
@@ -12,8 +12,10 @@ import GameplayKit
 public final class PlayerControlComponent: GameComponent {
     
     private let level: Level
-    public private(set) var direction: Direction? = nil
-    public var attemptedDirection: Direction? = nil
+    public private(set) var direction: Direction?
+    
+    public var attemptedDirection: Direction?
+    private var nextNode: GKGridGraphNode?
     
     public init(
         level: Level
@@ -31,5 +33,79 @@ public extension PlayerControlComponent {
     func resetDirection() {
         direction = nil
         attemptedDirection = nil
+    }
+    
+    
+    func setDirection(_ newDirection: Direction?) {
+        var proposedNode: GKGridGraphNode?
+        if isCurrentlyMoving {
+            proposedNode = nodeInDirection(newDirection, from: nextNode)
+        } else {
+            let currentNode = level.pathfindingGraph.node(atGridPosition: gameEntity.gridPosition)
+            proposedNode = nodeInDirection(newDirection, from: currentNode)
+        }
+        if proposedNode == nil { return }
+        self.direction = newDirection
+    }
+
+    
+    override func update(deltaTime _: TimeInterval) {
+        makeNextMove()
+    }
+}
+
+// MARK: Private
+private extension PlayerControlComponent {
+    func nodeInDirection(
+        _ newDirection: Direction?,
+        from node: GKGridGraphNode?
+    ) -> GKGridGraphNode? {
+        guard let newDirection = newDirection, let node = node else {
+            return nil
+        }
+        let nextPosition: GridPosition
+        switch newDirection {
+        case .left:
+            nextPosition = node.gridPosition + GridPosition.init(x: -1, y: 0)
+        case .right:
+            nextPosition = node.gridPosition + GridPosition.init(x: 1, y: 0)
+        case .down:
+            nextPosition = node.gridPosition + GridPosition.init(x: 0, y: -1)
+        case .up:
+            nextPosition = node.gridPosition + GridPosition.init(x: 0, y: 1)
+        }
+        return level.pathfindingGraph.node(atGridPosition: nextPosition)
+    }
+    
+    func makeNextMove() {
+        let currentNode = level.pathfindingGraph.node(atGridPosition: gameEntity.gridPosition)
+        if let attemptedNextNode = nodeInDirection(attemptedDirection, from: currentNode) {
+            // Move in the attempted direction.
+            direction = attemptedDirection
+            self.nextNode = attemptedNextNode
+            spriteComponent.setNextGridPosition(attemptedNextNode.gridPosition)
+            
+        } else if let nextNode = nodeInDirection(direction, from: currentNode) {
+            // Keep moving in the same direction.
+            self.nextNode = nextNode
+            spriteComponent.setNextGridPosition(nextNode.gridPosition)
+        } else {
+             // Can't move any more.
+            direction = nil
+        }
+    }
+    
+    var spriteComponent: SpriteComponent {
+        gameEntity.componentOf(type: SpriteComponent.self)
+    }
+
+    var isCurrentlyMoving: Bool {
+        direction != nil
+    }
+}
+
+public extension GridPosition {
+    static func + (lhs: GridPosition, rhs: GridPosition) -> GridPosition {
+        fatalError()
     }
 }
